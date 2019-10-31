88de7e694aac2319789bb12408376db2f1cd375b
luoriwusheng <1197802519@qq.com>
2019-10-31 10:49:25 +0800

    更新包 重新测试运行通过

--- lesson-4/webpack.dev.js
+++ lesson-4/webpack.dev.js
@@ -3,6 +3,7 @@ const merge = require('webpack-merge')
 const commen = require('./webpack.commen.js')
 
 module.exports = merge(commen, {
+  mode: "development",
 	devtool: 'inline-source-map',
 	devServer: {
 		contentBase: './dist'
--- lesson-4/webpack.prod.js
+++ lesson-4/webpack.prod.js
@@ -2,10 +2,13 @@
 const merge = require('webpack-merge')
 const commen = require('./webpack.commen.js')
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin")
+const {CleanWebpackPlugin} = require('clean-webpack-plugin'); // 清理打包的目录--使运行 npm run dev 生成的dist永远是最新的
 
 module.exports = merge(commen, {
+  mode: "production",
 	devtool: 'source-map',
 	plugins: [
+    new CleanWebpackPlugin(),
 		new UglifyJSPlugin({
 			sourceMap: true
 		})
